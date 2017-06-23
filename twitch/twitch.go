1f7073a359c15cbaa6f886d52a46cf8d72d8166b
João Marques <mrshankly@users.noreply.github.com>
2017-06-23 18:48:23 +0100

    Add helper function for inserting query parameters into an URL

--- twitch/twitch.go
+++ twitch/twitch.go
@@ -9,6 +9,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"reflect"
+
+	"github.com/google/go-querystring/query"
 )
 
 const (
@@ -39,6 +42,28 @@ type Client struct {
 
 type service struct {
 	client *Client
+}
+
+// Adds the parameters in opt as URL query parameters to s. opt must be a struct
+// whose fields may contain "url" tags.
+func addOptions(s string, opt interface{}) (string, error) {
+	v := reflect.ValueOf(opt)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return s, nil
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return s, err
+	}
+
+	qs, err := query.Values(opt)
+	if err != nil {
+		return s, err
+	}
+	u.RawQuery = qs.Encode()
+
+	return u.String(), nil
 }
 
 // Returns a new Twitch API client.
